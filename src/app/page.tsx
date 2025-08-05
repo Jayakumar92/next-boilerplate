63dfe6b74482a23c775601312976f2f9093300ac
JAYAKUMAR\jayak <jayakumar.developer@gmail.com>
2025-08-05 10:06:30 +0530

    boilerplate-code-structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center font-sans">
       <h1 className="text-lg font-semibold underline">Next boilerplate</h1>
-      <p className="text-sm">Next App with prettier, husky, lint-staged</p>
+      <p className="text-sm">
+        Next App with prettier, eslint, husky, lint-staged
+      </p>
     </div>
   )
 }
