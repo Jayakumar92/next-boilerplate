e883fc6a2351c8e23252c9fb26356838b68dcdb6
JAYAKUMAR\jayak <jayakumar.developer@gmail.com>
2025-08-05 19:31:11 +0530

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { setLanguage } from "@/redux-store/app"
 import { useTranslations } from "next-intl"
 
 import { useRedux } from "@/hooks"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   const t = useTranslations()
@@ -13,11 +14,12 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center font-sans">
       <div className="inline-flex items-center gap-2">
         <h1 className="text-lg font-semibold underline">{t("appName")}</h1>
-        <p
+        <Button
+          className="hidden cursor-pointer uppercase"
           onClick={() => dispatch(setLanguage(language === "ta" ? "en" : "ta"))}
         >
           {language}
-        </p>
+        </Button>
       </div>
       <p className="text-sm">
         Next App with prettier, eslint, husky, lint-staged
